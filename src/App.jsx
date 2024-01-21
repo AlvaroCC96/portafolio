b10f81592acf8cfa642259e0b60c1a7c5b001620
Álvaro Castillo <alvarolucascc96@gmail.com>
2024-01-21 18:03:17 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import { NavBar } from './components/NavBar/NavBar'
+import { Hero } from './components/Hero/Hero'
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <div className='container'>
+      <Hero/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <section className="hero-container">
+      <div className="hero-content">
+
+        <h2>Soy &Aacute;lvaro Castillo.</h2>
+        <p>
+        Soy un desarrollador FullStack proactivo, con una fuerte capacidad de adaptación al cambio y 
+        una disposici&oacute;n constante para aprender nuevas tecnolog&iacute;as relevantes en el mercado. 
+        Mi meta es contribuir significativamente en mi entorno de trabajo, aportando mis conocimientos y habilidades, 
+        adem&aacute;s de buscar un crecimiento continuo tanto a nivel personal como profesional.
+        </p>
+      </div>
+
+      <div className="hero-img">
+        <div>
+          <div className="tech-icon">
+            <img src="./assets/images/11.jpg" alt="" />
+          </div>
+          <img id="profile-img" src="./assets/images/11.jpg" alt="" />
+        </div>
+
+        <div>
+          <div className="tech-icon">
+            <img src="./assets/images/01.png" alt="" />
+          </div>
+          <div className="tech-icon">
+            <img src="./assets/images/01.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
